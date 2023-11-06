a10f8e95fdf752904731f7fb1de7e4c54cef3de4
lseong96 <lseong96@gmail.com>
2023-11-06 17:53:20 +0900

    dto

---
+++ src/main/java/com/cinema/admin/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.cinema.admin.dto;
+
+public class UserDTO {
+    private String uid;
+    private String name;
+    private String hp;
+    private String address;
+    private int point;
+    private String birth;
+    private String type;
+}
