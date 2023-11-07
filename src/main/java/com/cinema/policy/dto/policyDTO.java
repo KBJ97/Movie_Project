3e217cd3c7f6481767c53918ccfc59180a33fdfe
tngus78901 <tngus78901@gmail.com>
2023-11-07 17:33:07 +0900

    newsReport 테이블 추가

--- src/main/java/com/cinema/policy/dto/policyDTO.java
+++ src/main/java/com/cinema/policy/dto/policyDTO.java
@@ -1,4 +1,28 @@
 package com.cinema.policy.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class policyDTO {
+/* 신문고 제보 시 필요한 항목*/
+
+    /*제보내용*/
+    private int ano;
+    private String company;
+    private String cate;
+    private String title;
+    private String content;
+    private String file;
+
+    /*제보자*/
+    private String name;
+    private String contact;
+    private int phoneNumber;
+    private String email;
 }
