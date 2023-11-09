a2358cb71a4aaf79dcbd0291a3192f2a2ec38cde
lseong96 <lseong96@gmail.com>
2023-11-09 18:09:39 +0900

    register

--- src/main/java/com/cinema/admin/mapper/MovieMapper.java
+++ src/main/java/com/cinema/admin/mapper/MovieMapper.java
@@ -4,12 +4,14 @@ import com.cinema.admin.dto.FileDTO;
 import com.cinema.admin.dto.MovieDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface MovieMapper {
 
     public void insertMovie(MovieDTO movieDTO);
     public void insertFile(FileDTO fileDTO);
 
-
+    public List<MovieDTO> selectMovies();
 
 }
