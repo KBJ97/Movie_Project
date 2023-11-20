559e6828cac59719c0bf1d981cd4aae1e58efb8f
KBJ97 <sjn04037@naver.com>
2023-11-21 07:46:50 +0900

    로그인

--- src/main/java/com/cinema/entity/MemberEntity.java
+++ src/main/java/com/cinema/entity/MemberEntity.java
@@ -1,10 +1,14 @@
 package com.cinema.entity;
 
 import com.cinema.member.dto.MemberDTO;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
@@ -17,24 +21,38 @@ public class MemberEntity {
     @Id
     private String uid;
     private String name;
-    private String pass;
+    private String pass1;
     private String hp;
     private int gender;
     private int point;
-    private String birth;
-    private String type;
 
+    @Column(name = "birth")
+    private String birth;
+
+    private String type;
 
     public MemberDTO toDTO() {
         return MemberDTO.builder()
                 .uid(uid)
                 .name(name)
-                .pass(pass)
+                .pass1(pass1)
                 .hp(hp)
                 .gender(gender)
                 .point(point)
                 .birth(birth)
                 .type(type)
                 .build();
+    }
+
+    public void setBirthFromString(String birth) {
+        this.birth = convertToDatabaseColumn(birth);
+    }
+
+    public String convertToDatabaseColumn(String attribute) {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyMMdd");
+        LocalDate birthDate = LocalDate.parse(attribute, inputFormatter);
+
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return outputFormatter.format(birthDate);
     }
 }
