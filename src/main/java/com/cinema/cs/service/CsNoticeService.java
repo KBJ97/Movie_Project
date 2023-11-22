527b2e67dd8313612f82e08e562f681e7b40cfc7
KBJ <sjn04037@naver.com>
2023-11-22 11:51:00 +0900

    csNotice 페이징

--- src/main/java/com/cinema/cs/service/CsNoticeService.java
+++ src/main/java/com/cinema/cs/service/CsNoticeService.java
@@ -56,13 +56,12 @@ public class CsNoticeService {
 
     // 페이지 그룹
     public int[] getPageGroupNum(int currentPage, int lastPageNum) {
-        int currentPageGroup = (int)Math.ceil(currentPage / 10.0);
+        int currentPageGroup = (int) Math.ceil(currentPage / 10.0);
         int pageGroupStart = (currentPageGroup - 1) * 10 + 1;
         int pageGroupEnd = currentPageGroup * 10;
 
         // 페이지 그룹 마지막 넘버가 페이지 넘버보다 클 경우, 페이지 마지막 넘버 = 페이지 그룹 마지막 넘버
-        // ex) 페이지 넘버가 8이 마지막일 경우, 페이지 그룹 마지막 넘버가 10이 아니라 8이 됨.
-        if(pageGroupEnd > lastPageNum){
+        if (pageGroupEnd > lastPageNum) {
             pageGroupEnd = lastPageNum;
         }
 
@@ -73,23 +72,15 @@ public class CsNoticeService {
 
     // 페이지 시작번호
     public int getPageStartNum(int total, int currentPage) {
-        int start = (currentPage - 1) * 10;
-        return total - start;
-    }
-
-    // 현재 페이지 번호
-    public int getCurrentPage(String pg) {
-        int currentPage = 1;
-
-        if(pg != null){
-            currentPage = Integer.parseInt(pg);
-        }
-
-        return currentPage;
+        int pageSize = 10; // 페이지당 표시될 항목 수
+        int start = total - ((currentPage - 1) * pageSize);
+        return start;
     }
 
     // Limit 시작번호
     public int getStartNum(int currentPage) {
-        return (currentPage - 1) * 10;
+        int pageSize = 10; // 페이지당 표시될 항목 수
+        return (currentPage - 1) * pageSize;
     }
+
 }
