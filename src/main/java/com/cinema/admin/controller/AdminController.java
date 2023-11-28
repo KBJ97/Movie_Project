7285312ab448877b79b5ab955c57a14f7e01de3c
lseong96 <lseong96@gmail.com>
2023-11-28 12:33:20 +0900

    today

--- src/main/java/com/cinema/admin/controller/AdminController.java
+++ src/main/java/com/cinema/admin/controller/AdminController.java
@@ -1,15 +1,33 @@
 package com.cinema.admin.controller;
 
+import com.cinema.admin.dto.MovieInfoDTO;
+import com.cinema.admin.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@Log4j2
+@RequiredArgsConstructor
 @Controller
 public class AdminController {
 
-    @GetMapping(value= {"/admin","/admin/index"})
-    public String index() {
+    private final MovieService movieService;
 
-        return "/admin/board/index";
+    @GetMapping(value= {"/admin","/admin/index"})
+    public String index(Model model) {
+
+
+        List<MovieInfoDTO> movieList = movieService.selectMovieInfoList();
+
+        log.info("movieList", movieList);
+
+        model.addAttribute("movieList", movieList);
+
+        return "/admin/index";
     }
 
 
