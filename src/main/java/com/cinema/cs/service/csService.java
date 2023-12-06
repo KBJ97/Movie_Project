973c2f338e7d3b6c732357ce0c9f0cd0f6fd1567
lseong96 <lseong96@gmail.com>
2023-12-06 12:02:13 +0900

    머지

--- src/main/java/com/cinema/cs/service/csService.java
+++ src/main/java/com/cinema/cs/service/csService.java
@@ -1,4 +1,21 @@
 package com.cinema.cs.service;
 
-public class csService {
+import com.cinema.admin.dto.CsFaqDTO;
+import com.cinema.cs.mapper.CsFaqMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CsService {
+
+    @Autowired
+    private CsFaqMapper csFaqMapper;
+
+    public List<CsFaqDTO> selectFaqs(int start){
+        return csFaqMapper.selectFaqs(start);
+    }
+
+
 }
